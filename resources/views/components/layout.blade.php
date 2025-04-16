239690bcc614b82d6096193a356d63a09c5d5bef
hadi <hadi.hatoum.a@gmail.com>
2025-04-16 17:19:14 +0300

    initial commit

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,33 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
-
-<x-header></x-header>
-
-<body x-data="{ page: 'ecommerce', 'loaded': true, 'darkMode': true, 'stickyMenu': false, 'sidebarToggle': false, 'scrollTop': false }" x-init="darkMode = JSON.parse(localStorage.getItem('darkMode'));
-$watch('darkMode', value => localStorage.setItem('darkMode', JSON.stringify(value)))" :class="{ 'dark text-bodydark bg-boxdark-2': darkMode === true }">
-    <!-- ===== Preloader Start ===== -->
-    <div x-show="loaded" x-init="window.addEventListener('DOMContentLoaded', () => { setTimeout(() => loaded = false, 500) })"
-        class="fixed left-0 top-0 z-999999 flex h-screen w-screen items-center justify-center bg-white dark:bg-black">
-        <div class="h-16 w-16 animate-spin rounded-full border-4 border-solid border-primary border-t-transparent">
-        </div>
-    </div>
-
-    <!-- ===== Preloader End ===== -->
-
-    <!-- ===== Page Wrapper Start ===== -->
-    <div class="flex h-screen overflow-hidden">
-        <!-- ===== Sidebar Start ===== -->
-        <x-sidebar></x-sidebar>
-        <!-- ===== Sidebar End ===== -->
-
-        <!-- ===== Content Area Start ===== -->
-        <div class="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
-            {{ $slot }}
-        </div>
-        <!-- ===== Content Area End ===== -->
-    </div>
-    <!-- ===== Page Wrapper End ===== -->
-    <script defer src="{{ asset('tailadmin/build/bundle.js') }}"></script>
+<head>
+    <meta charset="UTF-8" />
+    <link rel="icon" type="image/svg+xml" href="{{asset('tailadmin/build/favicon.png')}}" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <script type="module" crossorigin src="{{asset('tailadmin/build/assets/index--S-7Ys7w.js')}}"></script>
+    <link rel="stylesheet" crossorigin href="{{asset('tailadmin/build/assets/index-CsJfSzk7.css')}}">
+</head>
+<body class="dark:bg-gray-900">
+<div id="root"></div>
 </body>
-
 </html>
+
