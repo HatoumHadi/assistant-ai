8cf04d8d57f00d4ca7f2de57f3fc5a01acb5a2af
hadi <hadi.hatoum.a@gmail.com>
2025-05-13 13:03:38 +0300

    change code for deployment

--- public/tailadmin/src/components/auth/RequireAuth.tsx
+++ public/tailadmin/src/components/auth/RequireAuth.tsx
@@ -13,7 +13,7 @@ export default function RequireAuth({ children }: { children: JSX.Element }) {
             return;
         }
 
-        const API_BASE_URL = import.meta.env.VITE_API_URL || "http://127.0.0.1:8000";
+        const API_BASE_URL = import.meta.env.VITE_API_URL || "https://doova.tools";
 
         axios.get(`${API_BASE_URL}/api/check`, {
             headers: {
