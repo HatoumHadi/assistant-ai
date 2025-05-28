0098d1228fdcb0edd43c27217383d38854a40e89
hadi <hadi.hatoum.a@gmail.com>
2025-05-28 10:24:24 +0300

    fix content of data in file

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8" />
     <link rel="icon" href="{{ asset('storage/favicon.ico') }}" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <script type="module" crossorigin src="{{asset('tailadmin/build/assets/index-C2z6_s3y.js')}}"></script>
+    <script type="module" crossorigin src="{{asset('tailadmin/build/assets/index-C6YQhtlP.js')}}"></script>
     <link rel="stylesheet" crossorigin href="{{asset('tailadmin/build/assets/index-DPJk1HZ8.css')}}">
 </head>
 <body class="dark:bg-gray-900">
